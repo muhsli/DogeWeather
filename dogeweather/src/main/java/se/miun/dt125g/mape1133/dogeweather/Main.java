61f5b90f7645c979ef640ea978a80e50d6d1034d
muhsli <muhdif@gmail.com>
2014-02-08 13:58:07 +0100

    Fixed the XML-parsing. Now parses all data and pushes it to the textviews.

--- dogeweather/src/main/java/se/miun/dt125g/mape1133/dogeweather/Main.java
+++ dogeweather/src/main/java/se/miun/dt125g/mape1133/dogeweather/Main.java
@@ -23,9 +23,11 @@ import javax.xml.parsers.ParserConfigurationException;
 
 public class Main extends Activity {
 
-    String temperatureValue ="yollo";
-    String windspeed =null;
-    String cloudiness =null;
+    int symbolValue = 1337;
+    String weather = "much";
+    String temperatureValue ="wow";
+    String windspeedValue ="such very";
+    String cloudinessValue ="many so";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,19 +36,34 @@ public class Main extends Activity {
 
         Typeface tf = Typeface.createFromAsset(getAssets(),
                 "fonts/comicsans.ttf");
-        TextView tv = (TextView) findViewById(R.id.titleText);
-        tv.setTypeface(tf);
+        TextView titleTV = (TextView) findViewById(R.id.titleText);
+        titleTV.setTypeface(tf);
+        TextView windTV = (TextView) findViewById(R.id.windText);
+        windTV.setTypeface(tf);
+        TextView rainTV = (TextView) findViewById(R.id.rainText);
+        rainTV.setTypeface(tf);
+        TextView temperatureTV = (TextView) findViewById(R.id.temperatureText);
+        temperatureTV.setTypeface(tf);
+        TextView cloudyTV = (TextView) findViewById(R.id.cloudyText);
+        cloudyTV.setTypeface(tf);
 
-        RetrieveWeatherData weather = new RetrieveWeatherData();
-        weather.start();
+        RetrieveWeatherData retrieveWeatherDataThread = new RetrieveWeatherData();
+        retrieveWeatherDataThread.start();
 
         try {
-            weather.join();
+            retrieveWeatherDataThread.join();
         } catch (InterruptedException ex){
             ex.printStackTrace();
         }
 
-    tv.setText(temperatureValue);
+        if (symbolValue == 4){
+            weather = "cloud";
+        }
+        titleTV.setText("wow");
+        windTV.setText("very windy "+windspeedValue+"M/s");
+        rainTV.setText("so "+weather+" wow");
+        temperatureTV.setText("wow " + temperatureValue + " degeres");
+        cloudyTV.setText("many cloud "+cloudinessValue+ " precent");
     }
 
 class RetrieveWeatherData extends Thread {
@@ -70,11 +87,23 @@ class RetrieveWeatherData extends Thread {
 
             NodeList locationsList = weatherCastXmlDocument.getElementsByTagName("location");
             Element location = (Element) locationsList.item(0);
+            Element location2 = (Element) locationsList.item(1);
+
+            NodeList symbolList = location2.getElementsByTagName("symbol");
+            Element symbol = (Element) symbolList.item(0);
+            symbolValue = Integer.parseInt(symbol.getAttribute("number"));
 
             NodeList temperatureList = location.getElementsByTagName("temperature");
             Element temperature = (Element) temperatureList.item(0);
-
             temperatureValue = temperature.getAttribute("value");
+
+            NodeList windspeedList = location.getElementsByTagName("windSpeed");
+            Element windspeed = (Element) windspeedList.item(0);
+            windspeedValue = windspeed.getAttribute("mps");
+
+            NodeList cloudinessList = location.getElementsByTagName("cloudiness");
+            Element cloudiness = (Element) cloudinessList.item(0);
+            cloudinessValue = cloudiness.getAttribute("percent");
 
 
         } catch (MalformedURLException e) {
